977dabd55b988d0221f142a249ee69c57fa4c681
Binekrasik <binekrasik@proton.me>
2024-11-26 18:08:35 +0100

    Add random word generation using word sets

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,10 +1,20 @@
 import './App.scss'
 import Typer from './components/typer/Typer'
 
+import dictEnglish from '../../resources/dictionary/english_insanity.json'
+const dictionary = dictEnglish as string[]
+
 const App = () => {
+    const words = new Array< string >
+
+    for ( let i = 0; i < 24; i++ )
+        words.push( dictionary[ Math.floor( Math.random() * dictionary.length ) + 1 ] )
+
     return (
         <div className='App' >
-            <Typer text={ `form by problem help please still most force play there for could around last field ball order house` } />
+            <Typer
+                text={ words.join( ' ' ) }
+            />
         </div>
     )
 }
