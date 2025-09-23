cbd05f5d0564804cd621827084915359a7c8f38f
Binekrasik <66784259+Binekrasik@users.noreply.github.com>
2025-09-23 23:59:30 +0200

    impr: adjust typing sound effects

--- src/lib/audio/sounds.ts
+++ src/lib/audio/sounds.ts
@@ -3,7 +3,7 @@ import { Howl } from 'howler'
 export type SoundType = 'type' | 'error' | 'select' | 'score'
 
 export const sounds: Record< SoundType, Howl > = {
-    type: new Howl({ src: 'assets/audio/interact.wav', volume: 0.5, preload: true }),
+    type: new Howl({ src: 'assets/audio/type.webm', volume: 0.2, preload: true }),
     error: new Howl({ src: 'assets/audio/error.wav', preload: true }),
     select: new Howl({ src: 'assets/audio/select.wav', volume: 0.2, preload: true }),
     score: new Howl({ src: 'assets/audio/score.wav', volume: 0.1, preload: true }),
