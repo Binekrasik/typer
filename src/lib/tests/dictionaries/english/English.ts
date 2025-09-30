dbfabc1fb3531b8d5bcdc8f606affbc61d60a08b
Binekrasik <66784259+Binekrasik@users.noreply.github.com>
2025-09-30 23:09:28 +0200

    impr: improve english word generation

--- src/lib/tests/dictionaries/english/English.ts
+++ src/lib/tests/dictionaries/english/English.ts
@@ -6,18 +6,26 @@ export class English extends Dictionary {
     #currentDataset: DictionaryWordsDataset = english10k
 
     nextWord () {
-        let random = this.getRandomIndex()
+        let random
 
-        if ( random === this.#lastIndex )
-            if ( random === this.#currentDataset.words.length - 1 ) {
-                random--
-            } else random++
+        while ( true ) {
+            random = this.getRandomIndex()
 
-        this.#lastIndex = random
-        return this.#currentDataset.words[ random ].toLowerCase()
+            if ( random === this.#lastIndex )
+                if ( random === this.#currentDataset.words.length - 1 ) {
+                    random--
+                } else random++
+
+            if ( this.#currentDataset.words[ random ].length > 4 )
+                if ( Math.random() * 10 > 5 )
+                    continue
+
+            this.#lastIndex = random
+            return this.#currentDataset.words[ random ].toLowerCase()
+        }
     }
 
     getRandomIndex() {
-        return Math.round( Math.pow( Math.random(), 4 ) * this.#currentDataset.words.length )
+        return Math.round( Math.pow( Math.random(), 6 ) * this.#currentDataset.words.length )
     }
 }
